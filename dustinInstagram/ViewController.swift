3dd2d571b9faa3999e631760c03dea9424501022
Dustin <p4569zz@gmail.com>
2020-08-04 18:22:27 +0900

    2020.08.04 get,post Data to Heroku by Parse

--- dustinInstagram/ViewController.swift
+++ dustinInstagram/ViewController.swift
@@ -12,26 +12,72 @@ import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var senderlb: UILabel!
+    
+    @IBOutlet weak var receiverlb: UILabel!
+    let message = String()
+    let receiver = String()
+    let sender = String()
+    let picture = [PFFile]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        let object = PFObject(className: "testObject")
-        object["name"] = "Dustin"
-        object["lastname"] = "Park"
-        object.saveInBackground { (done, error) in
-            if done {
-                print( "saved in server")
+        
+       
+        
+     
+    }
+    
+    
+    func parseData() {
+        //create a class / collectionview / table in heroku
+        // PFObject - to create table or some data in table
+        
+        //unwrap
+        let pictureData = imageView.image?.jpegData(compressionQuality: 0.5)
+        
+        let file = PFFile(name: "pictrue.jpg", data: pictureData!)
+        let table = PFObject(className: "Message")
+        
+        table["sender"] = "Dustin"
+        table["receiver"] = "Haeju"
+        table["picture"] = file
+        
+        table.saveInBackground { (success, error) in
+            if success {
+            print("saved in server")
+            } else {
+            print(error)
+            }
+        }
+    }
+    
+    func retreiveData() {
+        let information = PFQuery(className: "Message")
+        information.findObjectsInBackground { (objects, error) in
+            if error == nil {
+                for object in objects! {
+                    self.receiverlb.text = object["receiver"] as? String
+                    self.senderlb.text = object["sender"] as? String
+                    
+                    let imageFile = object["picture"] as? PFFile
+                    imageFile?.getDataInBackground (block: { (data, error) -> Void in
+                          if error == nil {
+                            if let imageData = data {
+                                self.imageView.image = UIImage(data:imageData)
+                                         }
+                          }
+                    })
+                    
+                }
+                
             } else {
                 print(error)
             }
         }
+        
     }
 
 
 }
 
-
-//  if done {
-//      print( "saved in server" )
-//  } else {
-//      print(error)
-//  }
